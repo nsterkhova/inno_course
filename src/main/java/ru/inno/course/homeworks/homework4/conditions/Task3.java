fe7e19283ad26db1eb289d4d9b56ebad131996c2
nsterkhova <nasterkhova@gmail.com>
2024-02-01 21:32:29 +0400

    Update Task3.java

--- src/main/java/ru/inno/course/homeworks/homework4/conditions/Task3.java
+++ src/main/java/ru/inno/course/homeworks/homework4/conditions/Task3.java
@@ -12,7 +12,7 @@ public class Task3 {
         int multipleOfFour = number % 4;
         if (isOdd == 1 && multipleOfThree == 0) {
             System.out.println("Нечетное число. Кратно трем.");
-        } else if (isOdd == 0 && multipleOfFour == 0){
+        } else if (isOdd == 0 && multipleOfFour == 0) {
             System.out.println("Четное число. Кратно четырем.");
         } else if (isOdd == 1) {
             System.out.println("Нечетное число");
